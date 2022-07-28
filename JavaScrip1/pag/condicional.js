8910dcdba2cb8edd02535a1951847936f0b50f6f
montefinal <103082560+montefinal@users.noreply.github.com>
2022-07-28 18:40:20 -0300

    Delete JavaScrip1 directory

--- JavaScrip1/pag/condicional.js
+++
@@ -1,7 +0,0 @@
-let tipoPokemon = prompt("Descubra el Pokemon misterioso");
-
-if((tipoPokemon == "Pikachu") || (tipoPokemon == "pikachu") || (tipoPokemon = "PIKACHU")) {
-    alert("Ganaste, el Pokemon era Pikachu!")
-}else{
-    alert("Perdiste, el Pokemon era Pikachu")
-}
